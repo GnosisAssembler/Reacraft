dfc599c7984c343628a013b4ea6f34fb2581bb09
pankaryp <pan.karyp@gmail.com>
2019-06-16 21:42:04 +0300

    Create stoneCheck.js

--- src/App.js
+++ src/App.js
@@ -7,12 +7,18 @@ import ironBlock from './assets/building/minerals/150px-Block_of_Iron.png';
 import diamondBlock from './assets/building/minerals/150px-Block_of_Diamond.png';
 import emeraldBlock from './assets/building/minerals/150px-Block_of_Emerald.png';
 import lapisBlock from './assets/building/minerals/150px-Lapis_Lazuli_Block.png';
+import stoneBrickBlock from './assets/building/stone/150px-Stone_Bricks.png';
+import mossyStoneBlock from './assets/building/stone/150px-Mossy_Stone_Bricks.png';
+import mossyCobbleBlock from './assets/building/stone/150px-Mossy_Cobblestone.png';
+import crackedStoneBlock from './assets/building/stone/150px-Cracked_Stone_Bricks.png';
+import chiseledStoneBlock from './assets/building/stone/150px-Chiseled_Stone_Bricks.png';
 
 // Import basic components
 import Minerals from './components/building/Minerals';
 
 // Import utils
 import { mineralCheckBlock, mineralCheckIngot } from './utils/mineralCheck';
+import { stoneCheckBlock, stoneCheckMat } from './utils/stoneCheck';
 
 class App extends Component {
   constructor(props) {
@@ -23,19 +29,31 @@ class App extends Component {
     };
   }
 
-  // onMineral
+  // onMineral event
   onMineral(e) {
     const id = e.target.id; //get id of image clicked
-
     // Call mineralCheck functions for blocks and ingots
     const mb = mineralCheckBlock(id);
     const mi = mineralCheckIngot(id);
-    
+    // Set state
     this.setState({
       main: mb,
       secondary:mi 
     });
-}
+  }
+
+  // onStone event
+  onStone(e) {
+    const id = e.target.id; //get id of image clicked
+    // Call stoneCheck functions for blocks and mats
+    const sb = stoneCheckBlock(id);
+    const sm = stoneCheckMat(id);
+    // Set state
+    this.setState({
+      main: sb,
+      secondary:sm 
+    });
+  }
   
   render() {
     return (
@@ -46,14 +64,21 @@ class App extends Component {
           <Minerals mineralMain={this.state.main} mineralSecondary={this.state.secondary}/>
         </div>
 
-        <h3>Building</h3>
+        <h2>Building</h2>
 
-        <h5>Minerals</h5>
+        <h3>Minerals</h3>
         <img src={goldBlock} alt="gold-block" width="36px;" onClick={this.onMineral.bind(this)} id="gold"/>
         <img src={ironBlock} alt="iron-block" width="36px;" onClick={this.onMineral.bind(this)} id="iron"/>
         <img src={diamondBlock} alt="diamond-block" width="36px;" onClick={this.onMineral.bind(this)} id="diamond"/>
         <img src={emeraldBlock} alt="emerald-block" width="36px;" onClick={this.onMineral.bind(this)} id="emerald"/>
         <img src={lapisBlock} alt="lapis-block" width="36px;" onClick={this.onMineral.bind(this)} id="lapis"/>
+
+        <h3>Stone</h3>
+        <img src={stoneBrickBlock} alt="stoneBrick" width="36px;" onClick={this.onStone.bind(this)} id="stoneBrick"/>
+        <img src={mossyStoneBlock} alt="mossyStone" width="36px;" onClick={this.onStone.bind(this)} id="mossyStone"/>
+        <img src={mossyCobbleBlock} alt="mossyCobble" width="36px;" onClick={this.onStone.bind(this)} id="mossyCobble"/>
+        <img src={crackedStoneBlock} alt="crackedStone" width="36px;" onClick={this.onStone.bind(this)} id="crackedStone"/>
+        <img src={chiseledStoneBlock} alt="chiseledStone" width="36px;" onClick={this.onStone.bind(this)} id="chiseledStone"/>
       
       </div>
     );
---
+++ src/utils/stoneCheck.js
@@ -0,0 +1,29 @@
+// Import block and ingot images
+import goldBlock from '../assets/building/minerals/150px-Block_of_Gold.png';
+import goldIngot from '../assets/materials/150px-Gold_Ingot.png';
+import ironBlock from '../assets/building/minerals/150px-Block_of_Iron.png';
+import ironIngot from '../assets/materials/150px-Iron_Ingot.png';
+import diamondBlock from '../assets/building/minerals/150px-Block_of_Diamond.png';
+import diamondIngot from '../assets/materials/150px-Diamond.png';
+import emeraldBlock from '../assets/building/minerals/150px-Block_of_Emerald.png';
+import emeraldIngot from '../assets/materials/150px-Emerald.png';
+import lapisBlock from '../assets/building/minerals/150px-Lapis_Lazuli_Block.png';
+import lapisIngot from '../assets/materials/150px-Lapis_Lazuli.png';
+
+/**
+ * Check the block
+ * @param eventStonel: String (the id of the clicked image)
+ */
+export function stoneCheckBlock(eventStone) {
+
+    
+}
+
+/**
+ * Check the mat
+ * @param eventStone: String (the id of the clicked image)
+ */
+export function stoneCheckMat(eventStone) {
+
+
+}
