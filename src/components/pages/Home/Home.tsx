604ff7d5871a8d0a57051a302b57e190ec60b6c9
pankaryp <x0pk@protonmail.com>
2022-09-19 02:00:55 +0300

    feat(workbench): create Workbench panel for normal items

--- src/components/pages/Home/Home.tsx
+++ src/components/pages/Home/Home.tsx
@@ -1,12 +1,53 @@
 import Item from 'components/molecules/Item/Item';
+import Workbench from 'components/organisms/Workbench/Workbench';
 
 import items from '../../../config/items.json';
 import styles from './Home.module.scss';
 
 const Home = () => {
+	const mockWorkbench = [
+		{
+			name: '',
+			iconUrl: ''
+		},
+		{
+			name: '',
+			iconUrl: ''
+		},
+		{
+			name: '',
+			iconUrl: ''
+		},
+		{
+			name: '',
+			iconUrl: ''
+		},
+		{
+			name: '',
+			iconUrl: ''
+		},
+		{
+			name: '',
+			iconUrl: ''
+		},
+		{
+			name: '',
+			iconUrl: ''
+		},
+		{
+			name: '',
+			iconUrl: ''
+		},
+		{
+			name: '',
+			iconUrl: ''
+		}
+	];
+
 	return (
 		<div>
 			<h1>Items</h1>
+			<Workbench coordinates={mockWorkbench} className={styles.workbench} />
 			<div className={styles.items}>
 				{items.blocks.map((block) => {
 					return <Item key={block.id} name={block.name} iconUrl={block.icon} className={styles.item} />;
